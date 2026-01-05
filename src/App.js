211b4926089a4b778bf4ea2bab10176b1242872c
maliksaadsajjad <mr.aantojdz@gmail.com>
2026-01-05 21:14:57 +0500

    Using contexts in the class-based components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import UserFinder from './components/UserFinder';
+import UsersContext from './store/users-context';
+
+const DUMMY_USERS = [
+  { id: 'u1', name: 'Max' },
+  { id: 'u2', name: 'Manuel' },
+  { id: 'u3', name: 'Julie' },
+];
 
 function App() {
+  const usersContext = {
+    users: DUMMY_USERS,
+  };
+
   return (
-    <div>
+    <UsersContext.Provider value={usersContext}>
       <UserFinder />
-    </div>
+    </UsersContext.Provider>
   );
 }
 
 export default App;
-
-// You can't use react hooks in class-based components
